109782f4b9c9e83a21ae21e194ea7ed457d6d5f8
Pramod Ghuge <pramod7@gmail.com>
2019-08-27 14:16:48 +0000

    Automated form submission & other tweaks
    automated form submission, added context menu & shortcut key, added slideshow, hid some form fields

--- background.js
+++ background.js
@@ -1,8 +1,34 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+console.log('eXTENSION starts!');
+
+function ReloadCommonShare() {
 	chrome.runtime.reload();
-	// if (tab.url.toString() == 'http://10.249.110.236/dashboard/') chrome.tabs.reload();
+	// Reload the tab
+	chrome.tabs.query({
+		url: 'http://10.249.110.236/dashboard/*'
+	}, function (tab) {
+		chrome.tabs.reload(tab[0].id);
+	});
+}
+
+chrome.contextMenus.create({
+	title: 'Reload SIM',
+	contexts: ['page'],
+	onclick: ReloadCommonShare
 });
 
-chrome.tabs.query({ url: 'http://10.249.110.236/dashboard/*' }, function(tab) {
-	chrome.tabs.reload(tab[0].id);
+// chrome.browserAction.onClicked.addListener(function (tab) {
+// 	ReloadCommonShare();
+// });
+
+chrome.commands.onCommand.addListener(function (command, tab) {
+	// Shortcut is triggered.
+	if (command === "case-review-shortcut") {
+		console.log('Command:', command);
+		chrome.tabs.sendMessage(tab.id, {
+			myMsg: 'Shortcut pressed'
+		}, function (response) {
+			// ...
+			console.log("Got a response!")
+		});
+	}
 });
